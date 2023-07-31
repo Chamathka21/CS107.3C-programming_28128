de21de2b92d4da56d71e57c64188f2099bdf8285
Chamathka21 <141057016+Chamathka21@users.noreply.github.com>
2023-07-31 22:07:27 +0530

    Add files via upload

---
+++ C# lab 5/Q2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__lab_day_05_Q_2
+{
+    internal class CalculateValues
+        private void sayHello()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
